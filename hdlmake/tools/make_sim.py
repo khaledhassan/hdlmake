eb10004a71dda0d277ff9030f1de769a02222ae8
Tristan Gingold <tristan.gingold@cern.ch>
2019-06-06 10:57:44 +0200

    fix incorrect check.

--- hdlmake/tools/make_sim.py
+++ hdlmake/tools/make_sim.py
@@ -13,9 +13,9 @@ from hdlmake.srcfile import VerilogFile, VHDLFile, SVFile
 
 def _check_simulation_manifest(manifest_dict):
     """Check if the simulation keys are provided by the top manifest"""
-    if not manifest_dict["sim_top"]:
+    if manifest_dict.get("sim_top") is None:
         raise Exception("sim_top variable must be set in the top manifest.")
-    if not manifest_dict["sim_tool"]:
+    if manifest_dict.get("sim_tool") is None:
         raise Exception("sim_tool variable must be set in the top manifest.")
 
 
